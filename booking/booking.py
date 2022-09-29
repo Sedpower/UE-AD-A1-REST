7d68477f771222669b1efd3f7ab25090fdc37f56
Benoit-Catta <csupremenight@gmail.com>
2022-09-29 19:11:11 +0200

    EXO 4 check avec ta version

--- booking/booking.py
+++ booking/booking.py
@@ -15,6 +15,38 @@ with open('{}/databases/bookings.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
+@app.route("/bookings", methods=['GET'])
+def get_json():
+   res = make_response(jsonify(bookings), 200)
+   return res
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid):
+   for book in bookings:
+      if(str(userid) == str(book["userid"])):
+         res = make_response(jsonify(book), 200)
+         return res
+   return make_response(jsonify({"error": "bad input parameter"}), 400)
+
+@app.route("/bookings/<userid>", methods=['POST'])
+def add_booking_byuser(userid):
+   req = request.get_json()
+   for book in bookings:
+      if(str(userid) == str(book["userid"])):
+         for dates in book["dates"]:
+            if (str(dates["date"]) == str(req["date"])):
+               for movie in dates["movies"]:
+                  if (str(movie) == str(req["movies"])):
+                     return make_response(jsonify({"error": "an existing item already exists"}), 409)
+               dates["movies"].append(req["movies"][0])
+               res = make_response(jsonify(book), 200)
+               return res
+         book["dates"].append(req)
+         res = make_response(jsonify(book), 200)
+         return res
+
+   return make_response(jsonify({"error": "bad input parameter"}), 400)
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
