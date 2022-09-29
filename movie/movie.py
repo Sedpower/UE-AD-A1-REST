136f93e2548ad072989a23ef9bc32bad0ad4e237
Sedpower <erwanchauvel44@orange.fr>
2022-09-29 15:31:21 +0200

    [TP1][feat] Implemented questions 1 and 2 of TP_VERT

--- movie/movie.py
+++ movie/movie.py
@@ -9,14 +9,106 @@ PORT = 3200
 HOST = '0.0.0.0'
 
 with open('{}/databases/movies.json'.format("."), "r") as jsf:
-   movies = json.load(jsf)["movies"]
+    movies = json.load(jsf)["movies"]
+
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
+
+
+@app.route("/template", methods=['GET'])
+def template():
+    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'), 200)
+
+
+@app.route("/json", methods=["GET"])
+def get_json():
+    return make_response(jsonify(movies), 200)
+
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def get_movie_byid(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            res = make_response(jsonify(movie), 200)
+            return res
+    return make_response(jsonify({"error": "Movie ID not found"}), 400)
+
+
+@app.route("/moviesbytitle", methods=['GET'])
+def get_movie_bytitle():
+    json = ""
+    if request.args:
+        req = request.args
+        for movie in movies:
+            if str(movie["title"]) == str(req["title"]):
+                json = movie
+
+    if not json:
+        res = make_response(jsonify({"error": "movie title not found"}), 400)
+    else:
+        res = make_response(jsonify(json), 200)
+    return res
+
+
+@app.route("/moviesbyrating", methods=["GET"])
+def get_movie_by_rate():
+    json = ""
+    if request.args:
+        req = request.args
+        for movie in movies:
+            if str(movie["rating"]) == str(req["rating"]):
+                if not json:
+                    json = [movie]
+                else:
+                    json.append(movie)
+
+    if not json:
+        res = make_response(jsonify({"error": "movie rating not found"}), 400)
+    else:
+        res = make_response(jsonify(json), 200)
+    return res
+
+
+@app.route("/movies/<movieId>", methods=['POST'])
+def create_movie(movieid):
+    req = request.get_json()
+
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            return make_response(jsonify({"error": "movie ID already exists"}), 409)
+
+    movies.append(req)
+    res = make_response(jsonify({"message": "movie added"}), 200)
+    return res
+
+
+@app.route("/movies/<movieid>/<rate>", methods=['PUT'])
+def update_movie_rating(movieid, rate):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movie["rating"] = float(rate)
+            res = make_response(jsonify(movie), 200)
+            return res
+
+    res = make_response(jsonify({"error": "movie ID not found"}), 201)
+    return res
+
+
+@app.route("/movies/<movieid>", methods=['DELETE'])
+def del_movie(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movies.remove(movie)
+            return make_response(jsonify(movie), 200)
+
+    res = make_response(jsonify({"error": "movie ID not found"}), 400)
+    return res
+
 
 if __name__ == "__main__":
-    #p = sys.argv[1]
-    print("Server running in port %s"%(PORT))
-    app.run(host=HOST, port=PORT)
+    # p = sys.argv[1]
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT, debug=True)
