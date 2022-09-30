2418f96c3cc4c310350b124eff491748ca8b284b
Sedpower <erwanchauvel44@orange.fr>
2022-09-30 14:40:14 +0200

    [TP1][feat] Added get booking of a user in users.py

--- user/user.py
+++ user/user.py
@@ -16,6 +16,17 @@ def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
 
+@app.route("/user/booking/<userId>/<date>", methods=["GET"])
+def get_user_booking_bydate(userId, date):
+    r = requests.get(f"http://localhost:3201/bookings/{str(userId)}")
+    if r.status_code != 200:
+        return make_response({"error": "bad input parameter"})
+    result = r.json()
+    for dateItem in result["dates"]:
+        if str(dateItem["date"]) == str(date):
+            return make_response(dateItem, 200)
+    return make_response({"error": "not found"}, 404)
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
