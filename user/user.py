fc91946f82e0fb2dffd2d62383a6d9ebd1b6e848
Benoit-Catta <csupremenight@gmail.com>
2022-09-30 14:27:55 +0200

    exo 5 get_movie_by_userid()

--- user/user.py
+++ user/user.py
@@ -16,6 +16,25 @@ def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
 
+@app.route("/user/booking/<userid>", methods=['GET'])
+def get_movie_by_userid(userid):
+
+   dates = requests.get('http://172.16.124.165:3201/bookings/'+userid)
+   if (str(dates) == '<Response [400]>'):
+      return make_response(jsonify({"error": "bad input parameter"}), 400)
+   dates = dates.json()
+   movie_tab = {'movies':[]}
+
+   for date in dates['dates']:
+      for movie in date["movies"]:
+         res_movie = requests.get('http://172.16.124.165:3200/movies/'+movie)
+         movie_tab['movies'].append(res_movie.json())
+
+   res = make_response(jsonify(movie_tab), 200)
+
+   return res
+
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
